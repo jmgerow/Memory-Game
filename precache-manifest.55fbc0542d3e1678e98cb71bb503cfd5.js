83b15a8278332153f2dc704706371bcf81db97e0
jmgerow <justingerow1@gmail.com>
2018-10-07 15:28:05 -0500

    Updates

---
+++ precache-manifest.55fbc0542d3e1678e98cb71bb503cfd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "2f038ed169c6da7ee22c",
+    "url": "/Memory-Game/static/js/main.2f038ed1.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/Memory-Game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "2f038ed169c6da7ee22c",
+    "url": "/Memory-Game/static/css/main.0db3b12a.chunk.css"
+  },
+  {
+    "revision": "c99434b9368ed9adb7dc7d6a6d367f11",
+    "url": "/Memory-Game/index.html"
+  }
+];
