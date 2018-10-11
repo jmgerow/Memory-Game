3abd5f7b07a3a95d555fbca9c08d900f41899ff4
jmgerow <justingerow1@gmail.com>
2018-10-10 20:08:34 -0500

    adjusted scoreboard responsiveness

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -4,9 +4,9 @@ import "./Scoreboard.css";
 const Scoreboard = props => (
  
     <div class="navbar sticky-top scoreboard">
-      <div class="col-sm-8 game-name">React Memory Game</div>  
-      <div class="col-sm-2 current-score">Current Score: {props.currentScore}</div>
-      <div class="col-sm-2">Top Score: {props.topScore}</div>
+      <div class="col-12 col-md-6 col-lg-8 game-name">React Memory Game</div>  
+      <div class="col-12 col-md-3 col-lg-2 current-score">Current Score: {props.currentScore}</div>
+      <div class="col-12 col-md-3 col-lg-2 top-score">Top Score: {props.topScore}</div>
     </div>
   
 );
